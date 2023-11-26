df7cade2e4d955dd0e9501cb9beb39b9450bb854
MARYNI-PC\Maryni <prikolchik574@gmail.com>
2023-11-26 22:18:42 +0200

    add v2 version add OneSingle SDK for Unity

--- Assets/Scripts/OneSignalExample.cs
+++ Assets/Scripts/OneSignalExample.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-//using OneSignalSDK;
+using OneSignalSDK;
 using UnityEngine;
 
 public class OneSignalExample : MonoBehaviour
@@ -8,7 +8,7 @@ public class OneSignalExample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       //OneSignal.Initialize("635c5f12-bb1c-4e8a-92a5-65636c604328"); 
+        OneSignal.Initialize("635c5f12-bb1c-4e8a-92a5-65636c604328"); 
     }
     
 }
