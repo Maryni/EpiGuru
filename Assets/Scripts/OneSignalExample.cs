bd40ffb1f7730bc04d5bdec3de077a29ff6517b4
MARYNI-PC\Maryni <prikolchik574@gmail.com>
2023-11-26 18:34:13 +0200

    Add OneSignal SDK fail to build

---
+++ Assets/Scripts/OneSignalExample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using OneSignalSDK;
+using UnityEngine;
+
+public class OneSignalExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       OneSignal.Initialize("635c5f12-bb1c-4e8a-92a5-65636c604328"); 
+    }
+    
+}
